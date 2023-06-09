b6a440c2db0bb7d55069c08eea39f1e5a73e24ed
Serg <107427220+Serblini@users.noreply.github.com>
2023-06-10 02:30:09 +0300

    DM

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ module.exports = new Sequelize(
   process.env.DB_NAME, // Название БД
   process.env.DB_USER, // Пользователь
   process.env.DB_PASSWORD, // ПАРОЛЬ
-  {
+  { dialectModule: require('pg'),
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
