0bac1f35b870985c3981d25d1029bfd5d21c35ee
Serg <107427220+Serblini@users.noreply.github.com>
2023-06-10 01:46:28 +0300

    make it better

--- db.js
+++ db.js
@@ -1,25 +1,25 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-   process.env.POSTGRES_URL,)
-
 // module.exports = new Sequelize(
-//   process.env.DB_NAME, // Название БД
-//   process.env.DB_USER, // Пользователь
-//   process.env.DB_PASSWORD, // ПАРОЛЬ
-//   {
-//     dialect: "postgres",
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
+//   //  process.env.POSTGRES_URL,)
 
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
+module.exports = new Sequelize(
+  process.env.DB_NAME, // Название БД
+  process.env.DB_USER, // Пользователь
+  process.env.DB_PASSWORD, // ПАРОЛЬ
+  {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 module.exports
   .authenticate()
