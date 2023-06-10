5145f714d320d9a802a6f78f0310d7820b3214f5
Serg <107427220+Serblini@users.noreply.github.com>
2023-06-10 03:06:11 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,13 +4,13 @@ const { Sequelize } = require("sequelize");
 //   //  process.env.POSTGRES_URL,)
 
 module.exports = new Sequelize(
-  process.env.DB_NAME, // Название БД
-  process.env.DB_USER, // Пользователь
-  process.env.DB_PASSWORD, // ПАРОЛЬ
+  process.env.PGDATABASE, // Название БД
+  process.env.PGUSER, // Пользователь
+  process.env.PGPASSWORD, // ПАРОЛЬ
   { dialectModule: require('pg'),
     dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
 
     dialectOptions: {
       ssl: {
